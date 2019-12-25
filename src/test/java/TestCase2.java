b5fc00a92a8cf35f98557dd0554b7519702c1077
Hanna <hanna.chaika@gmail.com>
2019-12-25 22:55:22 +0100

    TestCase2 with Firefox Driver is added

---
+++ src/test/java/TestCase2.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestCase2 {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\HP\\Documents\\udemy_courses\\Drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+    }
+}
