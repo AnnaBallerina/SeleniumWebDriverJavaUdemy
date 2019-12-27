b38e99b51ce63390155d09d56cd548272d42ff78
Hanna <hanna.chaika@gmail.com>
2019-12-27 23:33:54 +0100

    Webdriver basic methods

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -6,12 +6,13 @@ public class Demo {
     public static void main(String[] args) {
 
 //        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Documents\\udemy_courses\\Drivers\\chromedriver.exe");
-//        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-/*
-        driver.get("https://google.com");
-        System.out.println(driver.getTitle()); */
-        System.out.println("hey");
+
+        driver.get("https://google.com"); // Hit url on the browser
+        System.out.println(driver.getTitle()); // validate if your page title is correct
+
+        System.out.println(driver.getCurrentUrl()); // validate if you are landed at the right url
     }
 
 }
